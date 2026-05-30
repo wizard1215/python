d9a2fc79bccf2097ab10c155685b21cc84bcf6ef
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:28:06 +0530

    Add files via upload

---
+++ area of square.py
@@ -0,0 +1,34 @@
+#print the area of square:
+"""side=float(input("enter the side of sqauare:"))
+
+area=side*side
+
+print("The area of square is =", area)"""
+
+# take two float numbers as input and print thier sum:
+
+"""num1=float(input("enter the  first number:"))
+
+num2=float(input("enter the second number:"))
+sum=num1+num2
+
+average=sum/2
+print("the average of num1 and num2 is:",average)"""
+
+# take two a and b as input and print True if a is greater than b:
+"""a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if a>b:
+ print("True:")
+elif(b>a):
+ print("false")
+else:
+    print("both are equally:")"""
+
+"""# take three numbers as input and print their average:
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+c=float(input("enter the third number:"))
+sum=a+b+c
+average=sum/3
+print("the average of a,band c is =",average)"""
