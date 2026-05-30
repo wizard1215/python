5f5f4d73545c603660386b812e33fdb70d06850b
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:52:01 +0530

    Add files via upload

---
+++ fifth_prog.py
@@ -0,0 +1,11 @@
+# with function is used to close file automatically:
+"""with open("fifth.txt","r") as f:
+    data=f.read()
+    print(data)
+
+with open("fifth.txt","w") as f:
+    f.write("new data :")"""
+    
+
+
+
