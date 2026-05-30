a336a8dd296f8256d4a75b21fc6de0c469e3df51
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:43:35 +0530

    Add files via upload

---
+++ simple.intrest.py
@@ -0,0 +1,25 @@
+"""P="Principal"
+R="Rate"
+T="Time"
+
+"SIMPLE INTREST"=(P*R*T)/100"""
+
+"""principal=float(input("enter the prinipal ammount:"))
+
+rate=float(input("enter the rate of intrest:"))
+
+time=float(input("enter the time in month:"))
+
+simple_intrest=(principal*rate*time)/100
+
+print("the simple money is",principal)
+
+print("the simple intrest is:", simple_intrest)
+
+print("the total amount is:", principal + simple_intrest)"""
+
+
+
+
+
+
