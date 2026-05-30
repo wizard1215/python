df3531a454e876d1180cf84fd91cf5da3efd314a
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:25:41 +0530

    Add files via upload

---
+++ sum of two.py
@@ -0,0 +1,34 @@
+"""a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+print(a+b)"""
+
+"""a=10
+b=20
+c=15
+sum=a+b+c
+print(sum)"""
+
+""""a=10
+b=6
+diff=a-b
+print(diff)
+
+a=10
+b=5
+mul=a*b
+print(mul)
+
+a=10
+b=2
+divide=a/b
+print(divide)"""
+
+"""num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+print(num1/num2)"""
+
+
+
+
+
+
