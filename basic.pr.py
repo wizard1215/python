37de3e8dc97c972f1f3ee6636dd18c76ab3a3c8d
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:18:21 +0530

    Add files via upload

---
+++ basic.pr.py
@@ -0,0 +1,35 @@
+"""name='Ritik'
+age=21
+price=100.0
+village="shafipur"
+fathers_name="billu kumar"
+print("my name is :",name,"and i am:",age ,"years old:","i studying in b.tech 4th years :")
+print("my village is:",village,"and my fathers name is shri:",fathers_name)"""
+
+"""name=(input("enter your name:"))
+age=int(input("enter the age:"))
+price=float(input("enter he price:"))
+check=True
+b=None
+print(name)
+print(age)
+print(price)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(check))
+print(type(b))"""
+
+#wap to find the gretest number among three numbers:
+"""a=float(input("enter the  value of a:"))
+b=float(input("enter the value of b:"))
+c=float(input("enter the value of c:"))
+d=float(input("enter the value of d:"))
+if(a>b and a>c and a>d):
+    print("a is gretest:")
+elif(b>a and b>c and b>d):
+    print("b is laregest:")
+elif(c>d):
+    print("c is largest:")
+else:
+    print("d is largest:")"""
