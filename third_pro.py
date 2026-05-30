5d11a62bf1e43204cb4c03d69038511a93c2a89d
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:49:10 +0530

    Add files via upload

---
+++ third_pro.py
@@ -0,0 +1,11 @@
+# if we open as such file which is not exist in the folder 
+# hence the python atomatically cretade it into the folder as:
+
+"""f=open("ritik.txt","w")
+f.write()
+f.close()"""
+
+# second example:as
+"""f=open("smaple.txt","w")
+f.write()
+f.close()"""
