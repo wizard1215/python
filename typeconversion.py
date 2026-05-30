e08d13b90db1c34a163abcc5b76caa6985911dce
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:43:02 +0530

    Add files via upload

---
+++ typeconversion.py
@@ -0,0 +1,30 @@
+
+#IMPLICIT TYPE CASTING
+#CONVERT  INTEGER TO FLOAT VALUE
+"""X=int(10)
+Y=float(10.5)
+Z=X+Y
+print(Z,type(Z))"""
+
+#CONVERT  INTEGER TO COMPLEX VALUE
+"""x=15
+y=8+5j
+print(y)
+print(type(y))"""
+
+#convert float to complex
+"""x=10.5
+y=x+10j
+print(y,type(y))"""
+
+#EXPLICIT TYPE CASTING
+
+#convert integer to string
+"""int="90"
+str=int
+print(int,type(str))"""
+
+#convert integer to bollean
+x=
+y=bool(x)
+print(y,type(y))
