b5e09d40e2608803ee00a918c554242a444d88d6
Ritik_Uapdhyay <166151005+wizard1215@users.noreply.github.com>
2026-05-30 09:42:37 +0530

    Add files via upload

---
+++ Genrator.py
@@ -0,0 +1,21 @@
+#The genrator are the functions in python which are genrate the numbers or value one by one these are used:
+#the yield keywords:
+
+"""def numbers():
+   yield 1
+   yield 2
+   yield 3
+   yield 4
+
+for nums in numbers():
+   print(nums)"""
+
+
+"""def demo():
+    print("start:")
+    yield 10
+    print("middle:")
+    yield 20
+    print("end:")
+for i in demo():
+    print(i)"""
